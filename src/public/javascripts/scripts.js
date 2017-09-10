3535172511704231976a7984fd87d38a5ad725cf
wangandr <wangandr@seas.upenn.edu>
2017-09-09 21:30:13 -0400

    apparently spotify only supports playback for iOS and Android SDKs, not for their web API

--- src/public/javascripts/scripts.js
+++ src/public/javascripts/scripts.js
@@ -1,3 +1,6 @@
+var clientId = 'b56396e79c604c649633d03bb032c421';
+var clientsecret = 'eeaf9e7120e24ac397d37e59b8042403';
+
 function loadSong(videoID) {
     if (player) {
         player.loadVideoById(videoID, 0, "large");
@@ -19,10 +22,45 @@ function spotifyPlaySong() {
 	})
 }
 
+function getSpotifySavedSongs() {
+	var access_token = JSON.parse(localStorage.getItem('access_token'));
+	$.ajax({
+		type: 'GET',
+		url: 'https://api.spotify.com/v1/me/tracks',
+		headers: {
+              'Authorization': 'Bearer ' + access_token
+            },
+	})
+}
+
+function getSpotifyPlaylists() {
+	var access_token = JSON.parse(localStorage.getItem('access_token'));
+	console.log(access_token);
+	$.ajax({
+		type: 'GET',
+		url:'https://api.spotify.com/v1/me/playlists',
+		headers: {
+			'Authorization': 'Bearer ' + access_token
+		}
+	})
+}
+
+function authenticateSpotify() {
+	$.ajax({
+		type: 'GET',
+		url:'https://accounts.spotify.com/authorize',
+		data: {
+			'client_id': clientId,
+			'response_type': 'code',
+			'redirect_uri': 'http://localhost:3000'
+		}
+}
+
 $(document).ready(function() {
 	$('.playSpotify').each(function(i) {
 		$(this).click(function() {
-				spotifyPlaySong();
+			getSpotifyPlaylists();
 		});
 	});
 });
+
